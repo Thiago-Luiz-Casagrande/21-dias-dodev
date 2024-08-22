401d555ee13c3250ef2af6f416dc3181e1df12bd
Thiago Casagrande <thiagocasagrande2004@gmail.com>
2024-08-22 18:33:59 -0300

    criando pasta dia-6

---
+++ dia-6/exercicio dia-6/index.js
@@ -0,0 +1,13 @@
+let numero = Number(prompt('Insira um numero'))
+
+for(let contagem = 0; contagem <= numero; contagem++){
+    console.log('N*' + contagem)
+}
+
+for(let contagem2 = 0; contagem2 <= 50; contagem2 += 5){
+    console.log(contagem2)
+}
+
+for(let contagem2 = 50; contagem2 >= 0; contagem2 -= 5){
+    console.log(contagem2)
+}
---
+++ dia-6/index.js
@@ -0,0 +1,5 @@
+let tabuada = Number(prompt('insira o numero que deseja a tabuada'))
+
+for(let contador = 0; contador < 10; contador++){
+    console.log(tabuada + " x " + contador + " = " + tabuada*contador)
+}
